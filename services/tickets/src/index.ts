1771c972533af85bac6c4292f3ed4d5b7282bcd4
sz <shalominfinity@gmail.com>
2021-03-24 20:40:34 +0200

    first commit

--- services/tickets/src/index.ts
+++ services/tickets/src/index.ts
@@ -49,7 +49,7 @@ const start = async () => {
   }
 
   app.listen(3000, () => {
-    console.log('Listening on port 3000 on tickets apppp !!!!!!!!');
+    console.log('Listening on port 3000 on tickets apppp  !!!!!!!!');
   });
 };
 
