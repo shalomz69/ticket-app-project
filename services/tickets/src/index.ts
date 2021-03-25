1b494acea2dcbdca99bd30a328590a38d48a1cd7
sz <shalominfinity@gmail.com>
2021-03-25 14:31:31 +0200

    first commit

--- services/tickets/src/index.ts
+++ services/tickets/src/index.ts
@@ -37,7 +37,7 @@ const start = async () => {
 
     new OrderCreatedListener(natsWrapper.client).listen();
     new OrderCancelledListener(natsWrapper.client).listen();
-
+ 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
