04a2a039a65eb25a7bad3a471e18d10a2e593bc5
sz <shalominfinity@gmail.com>
2021-03-25 10:17:57 +0200

    first commit

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,8 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      //baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      baseURL: 'http://www.ticket-app-sz.website/',
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      //baseURL: 'http://www.ticket-app-sz.website/',
       headers: req.headers
     });
   } else {
