ec3428ed435a00433b8e16e7a3e7b25fc0c4f90d
sz <shalominfinity@gmail.com>
2021-04-05 10:24:35 +0300

    remove cors

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response,NextFunction  } from 'express';
+
 import { json } from 'body-parser';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -14,14 +15,20 @@ app.use(
   cookieSession({ signed: false, secure: false })
 );
 //app.use(cors());
+app.all('*', (req: Request, res: Response, next:NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header( "Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers: Content-Type, *");
+  next();
+});
 app.use(json());
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 app.use(currentUserRouter);
 
-app.all('*', () => {
-  throw new NotFoundError();
-});
+// app.all('*', () => {
+//   throw new NotFoundError();
+// });
 app.use(errorHandler);
 export { app };
