551bb0721a88dde3c148106102d36f41316cb5ee
sz <shalominfinity@gmail.com>
2021-04-05 10:31:31 +0300

    remove cors

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -17,7 +17,7 @@ app.use(
 //app.use(cors());
 app.all('*', (req: Request, res: Response, next:NextFunction) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header( "Access-Control-Allow-Credentials", true);
+  res.header( "Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers: Content-Type, *");
   next();
 });
