e5c3807b30585851836c9258818d5599319302e2
sz <shalominfinity@gmail.com>
2021-04-06 11:43:55 +0300

    cc

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -6,6 +6,7 @@ import { signupRouter } from './routes/signup';
 import { currentUserRouter } from './routes/current-user';
 import { errorHandler, NotFoundError } from '@szszsztickets/common';
 import cookieSession from 'cookie-session';
+import cors from 'cors'
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,6 +14,7 @@ app.use(
   cookieSession({ signed: false, secure: false })
 );
 app.use(json());
+app.use(cors())
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
