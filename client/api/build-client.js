29978143f9a2f2f43ba5ae373eac7c1277cda8c2
sz <shalominfinity@gmail.com>
2021-04-06 16:17:48 +0300

    cc

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -8,7 +8,7 @@ export default ({ req }) => {
       // baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       baseURL: 'http://www.ticket-app-sz.website/',
       headers: req.headers
-    });
+    }); 
   } else {
     // We must be on the browser
     return axios.create({
